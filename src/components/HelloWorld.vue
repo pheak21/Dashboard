4aee8a420cfcfb49912efb8b225afe4751c5b6dc
pheak21 <pheak7112@gmail.com>
2022-06-20 10:11:08 +0700

    update main

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -188,13 +188,13 @@
               status="exception"
             />
           </div>
-          <div class="demo-progress">
+          <!-- <div class="demo-progress">
             <el-progress type="circle" :percentage="0" />
             <el-progress type="circle" :percentage="25" />
             <el-progress type="circle" :percentage="100" status="success" />
             <el-progress type="circle" :percentage="70" status="warning" />
             <el-progress type="circle" :percentage="50" status="exception" />
-          </div>
+          </div> -->
         </el-main>
       </el-container>
     </el-container>
